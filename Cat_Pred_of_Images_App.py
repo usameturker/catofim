0bc5a32274e6741a8f50370095f6498c76ae7445
usameturker <57360854+usameturker@users.noreply.github.com>
2023-09-05 13:29:32 -0400

    Add files via upload

---
+++ Cat_Pred_of_Images_App.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import tempfile
+import os
+from urllib.request import urlopen
+
+st.title("Category Prediction of Images")
+
+uploaded_file = st.file_uploader("Upload an image from your PC", type=["jpg", "png", "jpeg"])
+image_url = st.text_input("Or paste the URL of an image")
+
+!pip install roboflow
+
+from roboflow import Roboflow
+
+rf = Roboflow(api_key="A0HwSSkoqIrnz9hxlrBs")
+project = rf.workspace().project("category")
+model = project.version(1).model
+
+if uploaded_file is not None:
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        temp_file.write(uploaded_file.read())
+
+    st.image(temp_file.name, caption="Uploaded Image", width=300)
+
+    jsonfile = model.predict(temp_file.name).json()
+    predicted_classes = str(jsonfile["predictions"][0]["predicted_classes"][0])
+    
+    st.subheader("Predicted Category:")
+
+    st.success(predicted_classes, icon="✅")
+
+    os.unlink(temp_file.name)  # Delete the temporary file
+
+elif image_url:
+    st.image(image_url, caption="Image from URL", width=300)
+
+    # Download the image from the URL
+    with urlopen(image_url) as response:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as temp_file:
+            temp_file.write(response.read())
+
+    jsonfile = model.predict(temp_file.name).json()
+    predicted_classes = str(jsonfile["predictions"][0]["predicted_classes"][0])
+    
+    st.subheader("Predicted Category:")
+
+    st.success(predicted_classes, icon="✅")
+
+    os.unlink(temp_file.name)  # Delete the temporary file
